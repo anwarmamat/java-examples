443c96a9fd2b50da34930ae8c35c3b9bc8a307ea
Anwar Mamat <anwar@umd.edu>
2018-02-20 21:30:38 -0500

    first commit

---
+++ 03/Travel.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ *    and (&&), or (||) example
+ */
+
+public class Travel {
+	public static void main(String[] args) {
+		
+		String name="alice";
+		boolean hasMoney = false;
+		boolean onVacation = false;
+		
+		if(hasMoney || onVacation) {		
+			System.out.println(name + " can travel.");
+		}else {
+			System.out.println(name + " stays.");
+		}
+	}
+
+}
